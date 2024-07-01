8d431d58edc586d0fb4f7833e6f3ded40e335415
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2024-07-01 22:21:20 +0300

    - diet plan noes screen done

--- lib/features/diet_plan/ui/diet_plan_notes_screen.dart
+++ lib/features/diet_plan/ui/diet_plan_notes_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kimofit/core/constants/colors.dart';
 import 'package:kimofit/core/widgets/custom_app_bar.dart';
+import 'package:kimofit/core/widgets/show_title_with_list.dart';
 import 'package:kimofit/generated/l10n.dart';
 
 class DietPlanNotesScreen extends StatelessWidget {
@@ -13,6 +15,15 @@ class DietPlanNotesScreen extends StatelessWidget {
       backgroundColor: ColorsManager.bgDark,
       appBar: CustomAppBar(
         title: S.current.notes,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.h, horizontal: 30.w),
+          child: ShowTitleWithList(
+            title: S.current.notes,
+            list: notes,
+          ),
+        ),
       ),
     );
   }
