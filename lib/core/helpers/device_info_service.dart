e2bd5b4324d46c39d169cc5f5ff958c30ead5444
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2024-07-11 14:23:35 +0300

    version: 1.0.10 - try to fix device id not unique by using serialNumber - using native code

--- lib/core/helpers/device_info_service.dart
+++ lib/core/helpers/device_info_service.dart
@@ -1,20 +1,50 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:flutter/services.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'dart:io';
 
 class DeviceUtils {
+  static const platform = MethodChannel('com.example.kimofit/deviceid');
+
   static Future<String> getDeviceId() async {
     DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
     String? deviceId;
+
     if (Platform.isAndroid) {
       AndroidDeviceInfo androidInfo = await deviceInfoPlugin.androidInfo;
-      deviceId = androidInfo.serialNumber;
+
+      String serial = androidInfo.supportedAbis.join(", ");
+
+      String androidId = androidInfo.id;
+
+      try {
+        String getAndroidId = await platform.invokeMethod('getAndroidId');
+        deviceId =
+            _generateUniqueId(androidId, serial, getAndroidId) + getAndroidId;
+      } on PlatformException catch (e) {
+        deviceId = 'Failed to get Android ID: ${e.message}';
+      }
     } else if (Platform.isIOS) {
       IosDeviceInfo iosInfo = await deviceInfoPlugin.iosInfo;
-      deviceId = iosInfo.identifierForVendor;
+
+      String? identifierForVendor = iosInfo.identifierForVendor ?? '';
+      String model = iosInfo.model;
+      String systemVersion = iosInfo.systemVersion;
+
+      deviceId = _generateUniqueId(identifierForVendor, model, systemVersion) +
+          identifierForVendor;
     } else {
       deviceId = 'unknown';
     }
 
-    return deviceId!;
+    return deviceId;
+  }
+
+  static String _generateUniqueId(
+      String id, String additionalInfo, String deviceId) {
+    var bytes = utf8.encode(id + additionalInfo + deviceId);
+    return sha256.convert(bytes).toString();
   }
 }
