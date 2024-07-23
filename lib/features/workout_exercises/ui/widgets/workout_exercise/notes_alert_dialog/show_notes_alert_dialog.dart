f3b23dbe6399f27123869a2b6ff2f243b9e4bd23
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2024-07-24 01:19:23 +0300

    - Added exercise notes alert dialog

---
+++ lib/features/workout_exercises/ui/widgets/workout_exercise/notes_alert_dialog/show_notes_alert_dialog.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kimofit/core/constants/colors.dart';
+import 'package:kimofit/features/workout_exercises/ui/widgets/notes_alert_dialog/notes_alert_dialog.dart';
+
+class ShownotesAlertDialog extends StatelessWidget {
+  const ShownotesAlertDialog({
+    super.key,
+    required this.notes,
+  });
+  final List<String> notes;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => notesAlertDialog(
+        context,
+        notes,
+      ),
+      child: Icon(
+        Icons.info,
+        size: 25.sp,
+        color: ColorsManager.white,
+      ),
+    );
+  }
+}
