2dabd4ed5782d130c01e8e355d99d33ca8734cbc
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2024-07-24 15:51:07 +0300

    - Added workout exercise

---
+++ lib/features/workout_exercises/ui/widgets/workout_exercise/workout_exercise.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kimofit/core/constants/colors.dart';
+import 'package:kimofit/core/helpers/extensions.dart';
+import 'package:kimofit/core/helpers/spacing.dart';
+import 'package:kimofit/core/routing/routes.dart';
+import 'package:kimofit/core/widgets/custom_rounded_network_image.dart';
+import 'package:kimofit/core/widgets/hero_animation.dart';
+import 'package:kimofit/features/workout_exercises/data/models/workout_exercise_model.dart';
+import 'package:kimofit/features/workout_exercises/ui/widgets/exercise_log/show_exercise_log.dart';
+import 'package:kimofit/features/workout_exercises/ui/widgets/workout_exercise/notes_alert_dialog/show_notes_alert_dialog.dart';
+import 'package:kimofit/features/workout_exercises/ui/widgets/workout_exercise/workout_exercise_name_and_sets.dart';
+
+class WorkoutExercise extends StatelessWidget {
+  const WorkoutExercise({
+    super.key,
+    required this.exercise,
+    required this.heroId,
+    this.isGroupExercise = false,
+  });
+
+  final WorkoutExerciseModel exercise;
+  final String heroId;
+  final bool isGroupExercise;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: isGroupExercise
+          ? EdgeInsets.only(bottom: 10.h)
+          : EdgeInsets.symmetric(vertical: 10.h, horizontal: 30.w),
+      child: GestureDetector(
+        onTap: () => context.pushNamed(
+          Routes.workoutExercisesDetailsScreen,
+          arguments: exercise,
+        ),
+        child: Container(
+          height: 100.h,
+          decoration: BoxDecoration(
+            color: ColorsManager.dark,
+            borderRadius: BorderRadius.circular(10.r),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                HeroAnimation(
+                  tag: heroId,
+                  child: CustomRoundedNetworkImage(
+                    height: 100.h,
+                    width: 80.w,
+                    imageUrl: exercise.image,
+                  ),
+                ),
+                horizontalSpace(10),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          WorkoutExerciseNameAndSets(
+                            name: exercise.name.getLocalizedText(),
+                            sets: exercise.logs.length.toString(),
+                          ),
+                          horizontalSpace(10),
+                          exercise.notes.isNotEmpty
+                              ? ShownotesAlertDialog(
+                                  notes: exercise.notes,
+                                )
+                              : const SizedBox.shrink(),
+                        ],
+                      ),
+                      ShowExerciseLog(
+                        name: exercise.name.getLocalizedText(),
+                        logs: exercise.logs,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
